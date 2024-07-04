45afea88cf56436cbda2f06960db16ff52a9888b
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-04 23:00:36 +0600

    simple mat form

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FirstCompComponent} from "./first-comp/first-comp.component";
+import {DynamicFormComponent} from "./dynamic-form/dynamic-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstCompComponent],
+  imports: [RouterOutlet, DynamicFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- my-app/src/app/dynamic-form/dynamic-form.component.ts
+++ my-app/src/app/dynamic-form/dynamic-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-dynamic-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './dynamic-form.component.html',
   styleUrl: './dynamic-form.component.css'
 })
