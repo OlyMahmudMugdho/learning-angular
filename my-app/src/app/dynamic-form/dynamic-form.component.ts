94febcce445d2bfb67bc407e0c4ea26cd13ddbca
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-05 16:57:35 +0600

    form comp

--- my-app/src/app/dynamic-form/dynamic-form.component.ts
+++ my-app/src/app/dynamic-form/dynamic-form.component.ts
@@ -2,18 +2,22 @@ import { Component } from '@angular/core';
 import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormModel} from "../models/form.model";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-dynamic-form',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButton],
   templateUrl: './dynamic-form.component.html',
   styleUrl: './dynamic-form.component.css'
 })
 
 
 export class DynamicFormComponent {
-  name: string = "Mugdho";
-  placeholder:string = "Ex. Mahmud";
-  label: string = "Full Name"
+  placeholder: FormModel = {
+    name: "your name",
+    email : "email@example.com",
+    age: 23
+  }
 }
