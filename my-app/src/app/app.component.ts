4490e4b399e48670e40781b35e36c4a826c128d9
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-05 18:11:08 +0600

    app module styling

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FirstCompComponent} from "./first-comp/first-comp.component";
 import {DynamicFormComponent} from "./dynamic-form/dynamic-form.component";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DynamicFormComponent],
+  imports: [RouterOutlet, DynamicFormComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'my-app';
 }
